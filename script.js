ab14d54b596453d14c996c3d39ba6b4cd9cc6971
AndiZogaj22 <az51520@ubt-uni.net>
2022-07-24 14:59:30 +0200

    ushtrime javascript

---
+++ script.js
@@ -0,0 +1,53 @@
+// This is a comment
+
+const variable = "Hello!";
+
+/*
+This is also a comment 
+*/
+console.log("hello world")
+
+console.log("My Name is Andi Zogaj")
+
+
+//DATA TYPES NE JAVASCRIPT
+
+
+//Strings
+console.log("Ky esht string".length)
+console.log('ky eshte string'.toUpperCase())
+console.log(`ky eshte nje stringn`.toLowerCase())
+console.log("Ku eshte shkronja e par A ne kete string".indexOf("u"))
+console.log("String me slice".slice("6")) 
+console.log("String me split".split(""))
+
+//numbers
+console.log(155)
+
+console.log(1+2 + "KaterString") //mbledhje numrave + 1 string 
+
+//boolean
+
+console.log(1 > 2)
+console.log(155 == 155)
+
+//null and undefined
+console.log(undefined)
+console.log(null)
+//null pa vler
+//undefined e pa definuar
+
+//symbols
+
+
+//typeof
+console.log(typeof "qka eshte kjo")
+
+
+//type coercion (try to avoid them)
+console.log("1" + 1)
+console.log("12" == 12) //numri 12 string eshte i barabart == me numrin 12 
+
+let a = "1"
+console.log(a + 1)
+console.log(++a)
---
+++ script2.js
@@ -0,0 +1,40 @@
+//Ushtrime JavaScript 
+//Task 1 write and display a string with the text Greetings !
+console.log("Greeting !")
+
+//Task 2 write and display a string that usess interpolation
+const mosha = 20
+console.log("Un" + "jam" + 20 + "vjeq !")
+
+// Task 3 write and display a string that spans multiple lines of code 
+console.log("Ky string ","ka shum rreshta kod ")
+
+// task 4 write string greetings but capitalised
+console.log("greetings !".toLocaleUpperCase())
+
+//task 5 check how manny characters are in the line greetings
+console.log("greetings !".length)
+
+//task 6 split the second line into two at the ";" character
+console.log("greetings friend".slice('10'))
+
+//task 7 perform a basic math calculation
+var number1 = 50
+var number2 = 40
+console.log(number2 + number1)
+
+//task 8 check if my result is a number and not a string 
+console.log(number1 + number2.typeof)/* duhet edhe niher me e kqyr */
+
+//task 9 perform a boolean check
+let one = 1
+let two = 2 
+console.log(one === two)
+console.log(one == two)
+console.log(one === one)
+
+//task 10
+
+
+
+//task 11 
