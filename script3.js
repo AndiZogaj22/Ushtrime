7da8c88ef6ff1fd39dbb1692da426ba542cad95d
AndiZogaj22 <az51520@ubt-uni.net>
2022-07-26 19:47:08 +0200

    ushtrime tjera nga kursi

---
+++ script3.js
@@ -0,0 +1,34 @@
+//basic operators
+/*
+addition + (MBLEDHJE)
+subraction - (ZBRITJE)
+multiplication * (SHUMZIM)
+division / (PJESTIM)
+*/
+console.log(20 + 20)
+console.log(20 - 20)
+console.log(20 * 20)
+console.log(20 / 20)
+
+//variable jane boxes ku mundemi me shti te dhana ka 3 menyra per te deklaruar variable 
+
+const firstvariable = "emri";
+
+let  secondvariable = "mbiemri";
+
+var thirdvariable = 2002;
+
+console.log(firstvariable)
+console.log(secondvariable)
+console.log(thirdvariable)
+
+
+//template literals
+console.log(`1+1=${1+1}`)
+//nese dojm me evaluate psh numra brenda stringu dhe me e pengu qe aj string mu print si vetem string perdorim ne ket rast ${1+1} 
+const name = "andi"
+console.log(`Hello ${name.toUpperCase()}!`)
+
+//
+let emri = 1
+console.log(emri)
---
+++ script4.js
@@ -0,0 +1,4 @@
+
+let frutiDy = "molla";
+let qmimi = 2
+console.log(`Hello ${frutiDy.toUpperCase()}! kushton ` ,qmimi,"euro")
