cd72b1d726e8b220701f795684beb1f5115a1020
AndiZogaj22 <az51520@ubt-uni.net>
2022-08-08 23:20:35 +0200

    ushtrime te reja

---
+++ script5.js
@@ -0,0 +1,70 @@
+
+// arithmetic operators
+let orders = 0;
+
+orders++
+orders = orders + 20
+orders--
+console.log(`we have ${orders} orders`)
+console.log(4 % 3) // me pjestu
+console.log(2 ** 3) // me shtu ne fuqi
+console.log(2 * 3)
+
+
+//Assigment operators
+let numri = 0
+numri += 10    //+= do te thot qe e shton shumen e deshiruar te barazimi i variables pra 10 plus barazimi qe eshte 0 shuma do jet 10
+numri -= 10
+numri *= 10
+numri /= 10
+numri **= 12
+console.log(numri)
+
+
+//comparison operators 
+const number1 = 1;
+const number2 = 2;
+let string1 = "1"
+let string2 = "2"
+
+
+console.log(number1 == number2) //del false sepse numeri 1 nuk eshte i barabart me numrin 2 
+console.log(string1 != string2) // del true sepse string1 nuk eshte i njejt me string2
+
+
+
+
+// == 
+// !=
+// ===
+// !== 
+// > 
+// >=
+// <
+// <=
+
+
+/*
+let number1 = 1
+let number2 = 2
+let string1 = "string1"
+let string2 = "string2"
+
+console.log(1 == "1")
+console.log(1 == "2")
+
+console.log(string1 === string2)
+console.log(string1 == string1)
+
+console.log(number1 != number2)
+
+console.log(number1 !== number2)
+
+console.log(string1 < string2)
+console.log(string1 > string2)
+*/
+
+//operators precedence 
+console.log((2+2)* 2) // nje her kryhet veprimi brenda kllapave pastaj ne kllapat tjerra
+
+
---
+++ script6.js
@@ -0,0 +1,21 @@
+// logical operators
+
+
+
+
+//AND &&
+console.log(true && true)  // a eshte true DHE true true po ato jan true 
+const test = "this is a test"
+console.log(typeof test === "string" && test.length > 50) // true sepse test eshte string
+
+
+//OR  || 
+console.log(true || true)
+console.log (false || false )
+
+
+//NOT !
+console.log(test !== "This is a test")
+
+
+//CONDITIONS
